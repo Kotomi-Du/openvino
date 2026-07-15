3236597ead69c337c7a9f6f2d6106c109fa871ce
Kotomi-Du <yaru.du@intel.com>
2026-07-14 18:23:05 -0700

    Revert "[GPU] Enable sdpa_micro for bidirectional attention and sliding window (#36737)"
    This reverts commit dd39abbc1762c2d8f306d0443c68f5c68eb4888e.

--- src/plugins/intel_gpu/tests/unit/test_cases/paged_attention_gpu_test.h
+++ src/plugins/intel_gpu/tests/unit/test_cases/paged_attention_gpu_test.h
@@ -1822,7 +1822,6 @@ public:
     struct gpu_outputs {
         std::map<cldnn::primitive_id, cldnn::network_output> outputs;
         cldnn::memory::ptr key_cache_mem;
-        cldnn::network::ptr network;
     };
 
     gpu_outputs run_gpu_inference(PagedAttentionManager& pam, T& p) {
@@ -2171,7 +2170,6 @@ public:
         last_block_indices_begins = pam.block_indices_begins;
 
         result.outputs = network->execute();
-        result.network = network;
 
         last_output_data_mem = result.outputs.at("output_data").get_memory();
 
